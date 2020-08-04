13c426a39f3459ec914ca9e479e33cff63606a6e
Erick-SN <ericksn0707@gmail.com>
2020-08-04 02:01:35 -0500

    Fetching and table component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { FormControl, Select, MenuItem } from '@material-ui/core';
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from '@material-ui/core';
 import InfoBox from './components/InfoBox';
 import Map from './components/Map';
+import Table from './components/Table';
 import './App.css';
+
 function App() {
   const [countries, setCountries] = useState([]);
+
   const [country, setCountry] = useState('worldwide');
+
+  const [countryInfo, setCountryInfo] = useState({});
+
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://disease.sh/v3/covid-19/all`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
   useEffect(() => {
     const getCountries = async () => {
       await fetch(`https://disease.sh/v3/covid-19/countries`)
@@ -15,40 +37,77 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
+          setTableData(data);
           setCountries(countries);
         });
     };
     getCountries();
   }, []);
-  const onCountryChange = (e) => {
+
+  const onCountryChange = async (e) => {
     const countryCode = e.target.value;
     // console.log(countryCode);
     setCountry(countryCode);
+    const URL =
+      countryCode === 'worldwide'
+        ? `https://disease.sh/v3/covid-19/all`
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
   };
+
   return (
-    <div className='App'>
-      <div className='app__header'>
-        <h1>COVID-19</h1>
-        <FormControl className='app__dropdown'>
-          <Select variant='outlined' onChange={onCountryChange} value={country}>
-            <MenuItem value='worldwide'>Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem key={country.name} value={country.value}>
-                {country.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+    <div className='app'>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19</h1>
+          <FormControl className='app__dropdown'>
+            <Select
+              variant='outlined'
+              onChange={onCountryChange}
+              value={country}>
+              <MenuItem value='worldwide'>Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem key={country.name} value={country.value}>
+                  {country.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='app__stats'>
+          <InfoBox
+            title='Coronavirus Cases'
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title='Recovered Cases'
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title='Deaths Cases'
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+
+        <Map />
       </div>
-      <div className='app__stats'>
-        <InfoBox title='Coronavirus Cases' cases={1234} total={2000} />
-        <InfoBox title='Recovered Cases' cases={1234} total={2000} />
-        <InfoBox title='Deaths Cases' cases={1234} total={2000} />
-      </div>
-      <Map />
-      {/* table  */}
-      {/* Graph  */}
-      {/* map  */}
+
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live Cases by country</h3>
+          <Table countries={tableData} />
+          <h3>Worldwide cases</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
